21047f68fd38f7bb365b6b7fd05ee89e354743b5
pedrorafaell82 <147397090+pedrorafaell82@users.noreply.github.com>
2025-11-14 13:08:32 +0100

    Spring Setup

---
+++ src/main/java/com/tqs/soundshop/SoundshopApplication.java
@@ -0,0 +1,13 @@
+package com.tqs.soundshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tqs/soundshop/SoundshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqs.soundshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
